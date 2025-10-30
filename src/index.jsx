d39fd9163f8d4cfe93e16e7ca7aa11718be2cea4
Max <max.choheli@gmail.com>
2025-10-30 20:09:17 +0200

    browsepage render test

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
 import { socketService } from './services/socket.service'
-
 socketService.setup()
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
@@ -24,7 +23,4 @@ root.render(
 	</Provider>
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register()
